7beb74b7733605531b3c380218cea512c23bf462
Shruti Shetty <shetty_14august@yahoo.com>
2020-10-30 19:02:31 -0400

    Adding the selected for fetching the interviewer data

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -14,3 +14,22 @@ export function getAppointmentsForDay(state, day) {
   })
   return finalArray;
 }
+
+export const getInterviewersForDay = (state, day) => {
+  
+  const filteredDays = state.days.filter(oneDay => oneDay.name === day);
+  let interviewArray = [];
+  const finalArray = [];
+  filteredDays.forEach(element => {
+    interviewArray = element.interviewers;
+  })
+  interviewArray.forEach(currElement => {
+    for (const interviewerEntry in state.interviewers) {
+      if (state.interviewers[interviewerEntry].id === currElement) {
+        finalArray.push(state.interviewers[interviewerEntry]);
+      }
+    }
+  })
+  return finalArray;
+
+}
