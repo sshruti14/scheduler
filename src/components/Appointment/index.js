f93bfd5de782aeb94cd4ccb1c5b43c3334f45b49
Shruti Shetty <shetty_14august@yahoo.com>
2020-11-03 18:21:33 -0500

    Adding integration test cases to the Application

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -84,7 +84,7 @@ export default function Appointment(props) {
   }
 
   return (
-    <article className="appointment">
+    <article className="appointment" data-testid="appointment" >
       <Header time={props.time} />
       {mode === EMPTY && <Empty onAdd={add} />}
       {mode === SHOW && (
