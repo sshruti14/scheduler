d3c60ed537ad1bf260eee758510a5f2677dbfde6
Shruti Shetty <shetty_14august@yahoo.com>
2020-11-03 16:22:02 -0500

    Adding mock for the axios

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -10,5 +10,9 @@ afterEach(cleanup);
 it("defaults to Monday and changes the schedule when a new day is selected", () => {
   const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday"));
+  return waitForElement(() => getByText("Monday")).then(() => {
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
+  
 });
